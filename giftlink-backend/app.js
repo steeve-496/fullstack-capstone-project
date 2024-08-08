12da0d9fdfaab330afb27be7e41049d38ac3900b
steeve-496 <steevebinubaby496@gmail.com>
2024-08-08 09:02:12 -0400

    Added Search Routes.js

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -23,7 +23,7 @@ app.use(express.json());
 const giftRoutes = require('./routes/giftRoutes');
 
 // Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
-//const searchRoutes = require('./routes/searchRoutes'); // Uncomment and adjust if you have search routes
+const searchRoutes = require('./routes/searchRoutes');
 
 const pinoHttp = require('pino-http');
 const logger = require('./logger');
@@ -34,7 +34,7 @@ app.use(pinoHttp({ logger }));
 app.use('/api/gifts', giftRoutes);
 
 // Search API Task 2: add the searchRoutes to the server by using the app.use() method.
-//app.use('/api/search', searchRoutes); // Uncomment and adjust if you have search routes
+app.use('/api/search', searchRoutes);
 
 // Global Error Handler
 app.use((err, req, res, next) => {
