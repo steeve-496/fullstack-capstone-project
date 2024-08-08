1a8f12aed7ea2d5788773bc51564085f58a751f3
steeve-496 <steevebinubaby496@gmail.com>
2024-08-08 08:55:57 -0400

    Added API for gift service

--- giftlink-backend/models/db.js
+++ giftlink-backend/models/db.js
@@ -1,28 +1,28 @@
 // db.js
 require('dotenv').config();
-const MongoClient = require('mongodb').MongoClient;
+const { MongoClient } = require('mongodb');
 
 // MongoDB connection URL with authentication options
-let url = `${process.env.MONGO_URL}`;
+const url = `${process.env.MONGO_URL}`;
 
 let dbInstance = null;
 const dbName = "giftdb";
 
 async function connectToDatabase() {
-    if (dbInstance){
-        return dbInstance
-    };
+    if (dbInstance) {
+        return dbInstance;
+    }
 
-    const client = new MongoClient(url);      
+    const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
 
     // Task 1: Connect to MongoDB
-    // {{insert code}}
+    await client.connect();
 
     // Task 2: Connect to database giftDB and store in variable dbInstance
-    //{{insert code}}
+    dbInstance = client.db(dbName);
 
     // Task 3: Return database instance
-    // {{insert code}}
+    return dbInstance;
 }
 
 module.exports = connectToDatabase;
